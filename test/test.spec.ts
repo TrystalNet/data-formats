fe4867ba0a8389c5723463afd6dda1e5967758ec
Terry O'Neill <terry.oneill@trystal.net>
2016-07-12 21:16:21 -0600

    seems to work for 2015/Cloud files

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,9 +1,48 @@
-import * as M from '../src/whatever.ts'
+import {Node, Trist, Cloud} from '@trystal/interfaces'
 
-describe('it runs a test', () => {
-  it('gets hello test', () => {
-    var result = M.sayHello()
-    expect(result).toBe('hello')
-  })
+import {buildChain} from '@trystal/data-gen'
+import {revise} from '../src/trist-formats.ts'
+
+const A = 'A', B='B'
+const BOB = 'bob'
+
+function daysFrom(d:Date) {
+  const t0 = new Date('July 12, 2016').getTime()
+  const t1 = new Date(d).getTime()
+  return Math.round((t1 - t0) / 1000 / 3600 / 24)
+}
+
+describe('it does a simple revision to an empty server trist / ', () => {
+  let cloudTrist:Cloud.Trist  
+  beforeEach(() => cloudTrist = revise({}, {nodes:buildChain(A)}, BOB))
+  it('it has correct version',() => expect(cloudTrist.version).toBe(20150) )
+  it('has exactly one revision',() => expect(cloudTrist.revisions.length).toBe(1))
+  it('has the correct number of content records',() => expect(cloudTrist.contents.length).toBe(1))
+  it('has the correct number of map records',() => expect(cloudTrist.map.length).toBe(1))
+  it('has the expected content record',() => expect(cloudTrist.contents[0]).toEqual({id:A, text:A}))
+  it('has the expected map records',() => expect(cloudTrist.map).toEqual([{id:A, isDeleted:false}]))
+  it('has the correct author', () => expect(cloudTrist.revisions[0].authorId).toEqual(BOB))
+  it('has exactly 0 edits', () => expect(cloudTrist.revisions[0].edits.length).toEqual(0))
+  it('has exactly 1 add', () => expect(cloudTrist.revisions[0].adds.length).toEqual(1))
+  it('has exactly 0 deletes', () => expect(cloudTrist.revisions[0].dels.length).toEqual(0))
+  it('has a reasonable revision date', () => expect(daysFrom(cloudTrist.revisions[0].date)).toBeGreaterThan(-1))
+  it('has the expected add', () => expect(cloudTrist.revisions[0].adds).toEqual([A]))
+  afterAll(() => {  })
+})
+
+describe('it does a simple revision that has two lines / ', () => {
+  let cloudTrist:Cloud.Trist  
+  beforeEach(() => cloudTrist = revise({}, {nodes: buildChain('A.B')}, BOB))
+  it('it has correct version',() => expect(cloudTrist.version).toBe(20150) )
+  it('has exactly one revision',() => expect(cloudTrist.revisions.length).toBe(1))
+  it('has the correct number of content records',() => expect(cloudTrist.contents.length).toBe(2))
+  it('has the correct number of map records',() => expect(cloudTrist.map.length).toBe(2))
+  it('has the expected content records',() => expect(cloudTrist.contents).toEqual([{id:A, text:A},{id:B,text:B}]))
+  it('has the expected map records',() => expect(cloudTrist.map).toEqual([
+    {id:A, next:B, vnext:B, isDeleted:false},
+    {id:B, rlevel:1, isDeleted:false}
+    ]))
+  it('has the expected adds', () => expect(cloudTrist.revisions[0].adds).toEqual([A,B]))
+  afterAll(() => { })
 })
 
