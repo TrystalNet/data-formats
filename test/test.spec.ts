7d5334ded55bc8a925b691d661a9dc849ade0f8e
Terry O'Neill <terry.oneill@trystal.net>
2016-07-12 21:22:41 -0600

    rename procject to data-formats

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,7 @@
 import {Node, Trist, Cloud} from '@trystal/interfaces'
 
 import {buildChain} from '@trystal/data-gen'
-import {revise} from '../src/trist-formats.ts'
+import {revise} from '../src/data-formats.ts'
 
 const A = 'A', B='B'
 const BOB = 'bob'
